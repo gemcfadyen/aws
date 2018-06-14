2c7c1e79bd9a882492000564d46a6105c7e8c0db
gemcfadyen <gemcfadyen@hotmail.co.uk>
2018-06-14 11:19:52 +0100

    Skeleton project

---
+++ src/main/java/ltd/penny/projects/App.java
@@ -0,0 +1,13 @@
+package ltd.penny.projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
